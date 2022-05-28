e698e5f2320405cfd7ebc6698cc8f7f4e66f5228
Aaron Marquez <Aaron.Marquez@technicalsafetybc.ca>
2022-05-27 21:37:28 -0700

    initial upload

---
+++ src/ENSContract.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+/// @title ENS Project
+/// @author Aaron Marquez
+/// @notice This contract registers and stores a human-readable name to an ETH address. A miniature version of ENS
+contract ENSContract {
+    /// @notice When a name is already taken
+    error NameUnavailableError();
+
+    /// @notice When someone tries to modify a name they don't own
+    error NameAccessDenied();
+
+    mapping(string => address) public ensRegistry;
+    uint private registrySize;
+
+    event AddressRegistered(address indexed owner, string name);
+    event AddressReleased(address indexed owner, string name);
+
+    /// @notice Register a human-readable name to the caller's address
+    /// @param name human-readable name to be mapped to caller's address
+    /// @dev There's an upper bound for registrySize if registry gets larger than uint256
+    function register(string calldata name) external returns (bool) {
+        if (ensRegistry[name] != address(0)) {
+            revert NameUnavailableError();
+        }
+        ensRegistry[name] = msg.sender;
+        registrySize += 1;
+        emit AddressRegistered(msg.sender, name);
+        return true;
+    }
+
+    /// @notice Release owner's human-readable name
+    /// @param name human-readable name that maps to an address
+    /// @dev caller can only release their own address
+    function release(string calldata name) external returns (bool) {
+        if (ensRegistry[name] != msg.sender) {
+            revert NameAccessDenied();
+        }
+        delete ensRegistry[name];
+        emit AddressReleased(msg.sender, name);
+        return true;
+    }
+
+    /// @notice Returns the address registered to a given name
+    /// @param name A human-readable name
+    /// @return The address of the given name
+    function retrieve(string calldata name) external view returns (address) {
+        return ensRegistry[name];
+    }
+
+    /// @notice Returns the total number of registered addresses
+    /// @return The total number of items in registry
+    /// @dev Will return incorrect value if registry size gets larger than uint256
+    function retrieveRegistrySize() external view returns (uint) {
+        return registrySize;
+    }
+
+}
---
+++ test/ENSContract.t.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+import "forge-std/Test.sol";
+import "../src/ENSContract.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract ContractTest is Test {
+
+    ENSContract internal ensContract;
+    string internal constant SAMPLE_NAME = "shakespeare.eth"; 
+
+    event AddressRegistered(address indexed owner, string name);
+    event AddressReleased(address indexed owner, string name);
+
+    function setUp() public {
+        ensContract = new ENSContract();
+    }
+
+    /// TEST REGISTER FUNCTIONS ///
+
+    /// @notice Try to register a single constant name
+    function testRegisterOneName() public {
+        bool success = ensContract.register(SAMPLE_NAME);
+        assertTrue(success);
+    }
+
+    /// @notice Try to register multiple fuzzed names
+    function testRegisterName(string memory sampleName) public {
+        bool success = ensContract.register(sampleName);
+        assertTrue(success);
+    }
+
+    /// @notice Try to register a name that is already taken
+    function testRegisterTakenName() public {
+        ensContract.register(SAMPLE_NAME);
+        vm.expectRevert(ENSContract.NameUnavailableError.selector);
+        ensContract.register(SAMPLE_NAME);
+    }
+
+    /// TEST EMITS ///
+    
+    /// @notice Check that address registered event is emitted
+    function testExpectRegisterEmit() public {
+        vm.expectEmit(true, false, false, true);
+        emit AddressRegistered(address(this), SAMPLE_NAME);
+        ensContract.register(SAMPLE_NAME);
+    }
+
+    /// @notice Check that address release event is emitted
+    function testExpectReleaseEmit() public {
+        ensContract.register(SAMPLE_NAME);
+        vm.expectEmit(true, false, false, true);
+        emit AddressReleased(address(this), SAMPLE_NAME);
+        ensContract.release(SAMPLE_NAME);
+    }
+
+    /// TEST RETRIEVALS ///
+
+    /// @notice Try to retrieve a single constant name
+    function testRetrieveOneName() public {
+        ensContract.register(SAMPLE_NAME);
+        address owner = ensContract.retrieve(SAMPLE_NAME);
+        assertEq(owner, address(this));
+    }
+
+    /// @notice Try to get the total size of registry
+    function testGetRegistrySize() public {
+        uint size = ensContract.retrieveRegistrySize();
+        assertEq(size, 0);
+    }
+
+    /// @notice Try to get the total size of registry after multiple register calls
+    function testGetRegistrySizeFuzzed(uint8 numRegisters) public {
+        for (uint8 i=0; i < numRegisters; i++) {
+            ensContract.register(Strings.toString(i));
+        }
+        uint size = ensContract.retrieveRegistrySize();
+        assertEq(size, numRegisters);
+    }
+
+    /// TEST RELEASE FUNCTIONS ///
+
+    /// @notice A name can be released only by the owner
+    function testReleaseAsOwner() external {
+        ensContract.register(SAMPLE_NAME);
+        bool success = ensContract.release(SAMPLE_NAME);
+        assertTrue(success);
+    }
+
+    /// @notice Revert occurs when non-owner tries to release
+    function testReleaseAsNotOwner() external {
+        ensContract.register(SAMPLE_NAME);
+        vm.expectRevert(ENSContract.NameAccessDenied.selector);
+        vm.prank(address(0));
+        ensContract.release(SAMPLE_NAME);
+    }
+}
